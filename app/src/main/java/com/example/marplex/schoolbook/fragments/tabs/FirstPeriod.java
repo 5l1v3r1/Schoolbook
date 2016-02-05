f5d951221de426230d943666b37c9a74a5000945
Marplex <marcocimolai49@gmail.com>
2016-02-05 19:34:44 +0100

    Added FirstPeriod fragment

---
+++ app/src/main/java/com/example/marplex/schoolbook/fragments/tabs/FirstPeriod.java
@@ -0,0 +1,35 @@
+package com.example.marplex.schoolbook.fragments.tabs;
+
+
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.marplex.schoolbook.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class FirstPeriod extends Fragment {
+
+
+    public FirstPeriod() {
+        // Required empty public constructor
+    }
+
+    public static FirstPeriod newInstance() {
+        return new FirstPeriod();
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_first_period, container, false);
+    }
+
+
+}
